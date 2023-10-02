7c0e26d4646e2af454efc9f12dbbf5a28206c1ca
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2023-10-02 22:58:13 +0900

    📝 Todoコンポーネントの作成

---
+++ src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Todo = () => {
+  return (
+    <div className='panel'>
+        <div className='panel-heading'>Todo App</div>
+        <div className='panel-block'>ここにTodoを表示</div>
+        <div className='panel-block'>ここにTodoを表示</div>
+        <div className='panel-block'>ここにTodoを表示</div>
+        <div className='panel-block'>
+            <span>ここにTodo数を表示</span>
+        </div>
+    </div>
+  )
+}
