3c5d871caec0ff2643cf16ce3a3e9701ad8b98c3
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2023-10-02 23:13:04 +0900

    📝 dummyDataの作成

---
+++ src/dummyData.jsx
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const dummyData = [
+    {id: uuidv4(), text: 'JavaScriptを学習する', done: false},
+    {id: uuidv4(), text: 'Reactを学習する', done: false},
+    {id: uuidv4(), text: 'Reactを使用した制作物をデプロイする', done: false}
+]
