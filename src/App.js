75726956e5a37efc34a522a59d875ed21be7bd0e
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2023-10-02 23:01:26 +0900

    📝Todoコンポーネントのインポートと画面表示

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Todo } from './components/Todo';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App container is-max-desktop">
+      <Todo />
     </div>
   );
 }
