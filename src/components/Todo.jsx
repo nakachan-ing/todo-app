87f02fc3d2e7754ab1d57eced4c6c6fb233da237
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2023-10-04 23:16:43 +0900

    🎨 エンターキーを押したらTodo追加するハンドラ関数定義

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import { dummyData } from '../dummyData'
 import { TodoItem } from './TodoItem'
 import { Input } from './Input'
+import { v4 as uuidv4 } from 'uuid'
 
 export const Todo = () => {
-    const [items, setItem] = useState(dummyData)
+    const [items, setItems] = useState(dummyData)
     console.log(items)
 
     const handleChangeCheck = (checked) => {
@@ -14,13 +15,17 @@ export const Todo = () => {
             }
             return item
         })
-        setItem(newItems)
+        setItems(newItems)
+    }
+
+    const handleAddTodo = (text) => {
+        setItems([...items, {id: uuidv4(), text, done: false}])
     }
 
   return (
     <div className='panel'>
         <div className='panel-heading'>Todo App</div>
-        <Input />
+        <Input onAdd={handleAddTodo}/>
         {items.map((item) => (
             <TodoItem key={item.id} item={item} onCheck={handleChangeCheck}/>
         ))}
